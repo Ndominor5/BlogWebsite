343d97f7fcf7d439d5ff9f47d2898738a5459d7e
Ndominor5 <reyesminorf@guilford.edu>
2021-02-01 21:23:03 -0600

    Compose page added

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.get("/contact", function(req, res) {
   res.render("contact", {contactParagraph: contactContent});
 });
 
-
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
 
 
 app.listen(3000, function() {
